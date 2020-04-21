620c1d4818f7cb652e1e4b5a83ecf70dd45d5bf8
Davitz01 <50636090+Davitz01@users.noreply.github.com>
2020-04-21 17:18:33 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,19 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        ## Created a metriz object 
+        inv <- NULL                                         # initialize with NULL vector
+        setMatrix <- function(m) {                          # Created a new function for setting matrix
+        x <<- m
+        inv <<- NULL
+    }
+        getMatrix <- function() x                           # Created a new function to access the matrix
+        setInverse <- function(inverse) inv <<- inverse     # Created a function for inverse
+        getInverse <- function() inv                        # Created a function for getting inverse
+        
+        list(set = setMatrix, get = getMatrix,              # Created and return a list containing all function before made
+         setinverse = setInverse, 
+         getinverse = getInverse)               
 }
 
 
@@ -12,4 +24,18 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        inv <- x$getinverse()           # Save the cached value of inverse 
+        
+        if(!is.null(inv)) {         # checks if the the matrix has not null values and returns cache the inverse
+        return(inv)                
+    }
+         data <- x$get()                 # otherwise, fetches the new/updated matrix
+         inv <- solve(data, ...)         # Save the inverse of matrix fetched above in inv.
+         x$setinverse(inv)               # call the caches the value of inverse computed above
+         inv                             # returns the freshly computed value of inverse
 }
+
+
+
+
+
